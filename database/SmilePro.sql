2dabe30acbe444b73324a91c2cc057b60fc287bd
NimrodLoozar <nimrod.lobozarl.com>
2024-11-14 00:08:14 +0100

    Implement user authentication and authorization features

--- database/SmilePro.sql
+++ database/SmilePro.sql
@@ -1,80 +1,423 @@
-DROP DATABASE IF exists jamin_a;
-create database jamin_a;
-use jamin_a;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 13, 2024 at 11:05 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
--- Tabel voor producten
-CREATE TABLE Product (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Naam VARCHAR(255) NOT NULL,
-    Barcode VARCHAR(20) NOT NULL UNIQUE,
-    IsActief BIT DEFAULT 1, 
-    Opmerking VARCHAR(250), 
-    DatumAangemaakt DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
-    DatumGewijzigd DATETIME(6) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Tabel voor allergenen
-CREATE TABLE Allergeen (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Naam VARCHAR(255) NOT NULL,
-    Omschrijving TEXT,
-    IsActief BIT DEFAULT 1, 
-    Opmerking VARCHAR(250), 
-    DatumAangemaakt DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
-    DatumGewijzigd DATETIME(6) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
 
--- Tabel voor producten per allergeen
-CREATE TABLE ProductPerAllergeen (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    ProductId INT NOT NULL,
-    AllergeenId INT NOT NULL,
-    FOREIGN KEY (ProductId) REFERENCES Product(Id),
-    FOREIGN KEY (AllergeenId) REFERENCES Allergeen(Id),
-    IsActief BIT DEFAULT 1, 
-    Opmerking VARCHAR(250), 
-    DatumAangemaakt DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
-    DatumGewijzigd DATETIME(6) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Tabel voor leveranciers
-CREATE TABLE Leverancier (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Naam VARCHAR(255) NOT NULL,
-    ContactPersoon VARCHAR(255) NOT NULL,
-    LeverancierNummer VARCHAR(20) NOT NULL UNIQUE,
-    Mobiel VARCHAR(20),
-    IsActief BIT DEFAULT 1, 
-    Opmerking VARCHAR(250), 
-    DatumAangemaakt DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
-    DatumGewijzigd DATETIME(6) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+--
+-- Database: `smilepro`
+--
+DROP DATABASE IF EXISTS `smilepro`;
+created_at DATABASE `smilepro`;
+use `smilepro`;
+-- --------------------------------------------------------
 
--- Tabel voor producten per leverancier
-CREATE TABLE ProductPerLeverancier (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    LeverancierId INT NOT NULL,
-    ProductId INT NOT NULL,
-    DatumLevering DATETIME NOT NULL,
-    Aantal INT NOT NULL,
-    DatumEerstVolgendeLevering DATETIME NOT NULL,
-    FOREIGN KEY (LeverancierId) REFERENCES Leverancier(Id),
-    FOREIGN KEY (ProductId) REFERENCES Product(Id),
-    IsActief BIT DEFAULT 1, 
-    Opmerking VARCHAR(250), 
-    DatumAangemaakt DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
-    DatumGewijzigd DATETIME(6) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+--
+-- Table structure for table `appointments`
+--
 
--- Tabel voor magazijn
-CREATE TABLE Magazijn (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    ProductId INT NOT NULL,
-    VerpakkingsEenheid DECIMAL(5,2) NOT NULL,
-    AantalAanwezig INT,
-    FOREIGN KEY (ProductId) REFERENCES Product(Id),
-    IsActief BIT DEFAULT 1, 
-    Opmerking VARCHAR(250), 
-    DatumAangemaakt DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
-    DatumGewijzigd DATETIME(6) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `appointments` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `patient_id` bigint UNSIGNED NOT NULL,
+  `employee_id` bigint UNSIGNED NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `status` enum('Bevestigd','Geannuleerd') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `appointments_patient_id_foreign` (`patient_id`),
+  KEY `appointments_employee_id_foreign` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+
+CREATE TABLE IF NOT EXISTS `cache` (
+  `key` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expiration` int NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+
+CREATE TABLE IF NOT EXISTS `cache_locks` (
+  `key` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `owner` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expiration` int NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `communications`
+--
+
+CREATE TABLE IF NOT EXISTS `communications` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `patient_id` bigint UNSIGNED NOT NULL,
+  `employee_id` bigint UNSIGNED NOT NULL,
+  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sent_date` datetime NOT NULL DEFAULT '2024-11-13 22:26:06',
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `communications_patient_id_foreign` (`patient_id`),
+  KEY `communications_employee_id_foreign` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `patient_id` bigint UNSIGNED NOT NULL,
+  `streetname` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `housenumber` int NOT NULL,
+  `adition` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postalcode` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `place` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `contacts_email_unique` (`email`),
+  KEY `contacts_patient_id_foreign` (`patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE IF NOT EXISTS `employees` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `person_id` bigint UNSIGNED NOT NULL,
+  `nummber` int NOT NULL,
+  `employee_type` enum('Assistent','Mondhygiënist','Tandarts','Praktijkmanagement') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `specialisation` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `availability` json DEFAULT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `employees_nummber_unique` (`nummber`),
+  KEY `employees_person_id_foreign` (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `patient_id` bigint UNSIGNED NOT NULL,
+  `employee_id` bigint UNSIGNED NOT NULL,
+  `judgement` tinyint NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `feedback_patient_id_foreign` (`patient_id`),
+  KEY `feedback_employee_id_foreign` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE IF NOT EXISTS `invoices` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `patient_id` bigint UNSIGNED NOT NULL,
+  `treatment_id` bigint UNSIGNED NOT NULL,
+  `nummber` int NOT NULL,
+  `date` date NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `status` enum('Verzonden','Niet-Verzonden','Betaald','Onbetaald') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `invoices_nummber_unique` (`nummber`),
+  KEY `invoices_patient_id_foreign` (`patient_id`),
+  KEY `invoices_treatment_id_foreign` (`treatment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(12, '2024_11_13_201743_create_users_table', 1),
+(13, '0001_01_01_000001_create_cache_table', 2),
+(14, '2024_11_12_085514_add_two_factor_columns_to_users_table', 2),
+(15, '2024_11_12_085543_create_personal_access_tokens_table', 2),
+(16, '2024_11_12_090118_create_teams_table', 2),
+(17, '2024_11_12_090119_create_team_user_table', 2),
+(18, '2024_11_12_090120_create_team_invitations_table', 2),
+(19, '2024_11_13_201737_create_persons_table', 2),
+(20, '2024_11_13_201743_create_roles_table', 2),
+(21, '2024_11_13_201744_create_contacts_table', 2),
+(22, '2024_11_13_201744_create_employees_table', 2),
+(23, '2024_11_13_201744_create_patients_table', 2),
+(24, '2024_11_13_201745_create_appointments_table', 2),
+(25, '2024_11_13_201745_create_invoices_table', 2),
+(26, '2024_11_13_201745_create_treatments_table', 2),
+(27, '2024_11_13_201746_create_communications_table', 2),
+(28, '2024_11_13_201746_create_feedback_table', 2),
+(29, '2024_11_13_223553_create_people_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE IF NOT EXISTS `patients` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `person_id` bigint UNSIGNED NOT NULL,
+  `nummber` int NOT NULL,
+  `medical_file` text COLLATE utf8mb4_unicode_ci,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `patients_nummber_unique` (`nummber`),
+  KEY `patients_person_id_foreign` (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `people`
+--
+
+CREATE TABLE IF NOT EXISTS `people` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `middle_name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birth_date` date NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `notes` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persons`
+--
+
+CREATE TABLE IF NOT EXISTS `persons` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `infix` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_of_bith` date NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roles_user_id_foreign` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE IF NOT EXISTS `teams` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `personal_team` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teams_user_id_index` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team_invitations`
+--
+
+CREATE TABLE IF NOT EXISTS `team_invitations` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `team_id` bigint UNSIGNED NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `team_invitations_team_id_email_unique` (`team_id`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team_user`
+--
+
+CREATE TABLE IF NOT EXISTS `team_user` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `team_id` bigint UNSIGNED NOT NULL,
+  `user_id` bigint UNSIGNED NOT NULL,
+  `role` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `team_user_team_id_user_id_unique` (`team_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `treatments`
+--
+
+CREATE TABLE IF NOT EXISTS `treatments` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `employee_id` bigint UNSIGNED NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `costs` decimal(10,2) NOT NULL,
+  `status` enum('Behandeld','Onbehandeld','Uitgesteld') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `treatments_employee_id_foreign` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `person_id` bigint UNSIGNED NOT NULL,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `two_factor_secret` text COLLATE utf8mb4_unicode_ci,
+  `two_factor_recovery_codes` text COLLATE utf8mb4_unicode_ci,
+  `two_factor_confirmed_at` timestamp NULL DEFAULT NULL,
+  `is_signd_in` tinyint(1) NOT NULL DEFAULT '0',
+  `signed_in` datetime DEFAULT NULL,
+  `signed_out` datetime DEFAULT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `comments` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_username_unique` (`username`),
+  KEY `users_person_id_foreign` (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
