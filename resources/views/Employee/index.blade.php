99a4e19b3dd5a5995e239dc975eea5ff3e8579b9
NimrodLobozar <nimrod.lobozar@gmail.com>
2024-12-18 12:10:27 +0100

    Refactor Employee index view for improved layout and consistency

--- resources/views/Employee/index.blade.php
+++ resources/views/Employee/index.blade.php
@@ -19,50 +19,51 @@
                 NEW +
             </a>
         </div>
+    </x-slot>
 
-        <table class="min-w-full bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-            <thead>
-                <tr class="bg-gray-700">
-                    <th class="py-2 px-4 text-left text-gray-200">Name</th>
-                    <th class="py-2 px-4 text-left text-gray-200">Number</th>
-                    <th class="py-2 px-4 text-left text-gray-200">Type</th>
-                    <th class="py-2 px-4 text-left text-gray-200">Specialization</th>
-                    <th class="py-2 px-4 text-left text-gray-200">Actions</th>
+    <table class="mt-4 min-w-full mx-auto bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
+        <thead>
+            <tr class="bg-gray-700">
+                <th class="py-2 px-4 text-left text-gray-200">Name</th>
+                <th class="py-2 px-4 text-left text-gray-200">Number</th>
+                <th class="py-2 px-4 text-left text-gray-200">Type</th>
+                <th class="py-2 px-4 text-left text-gray-200">Specialization</th>
+                <th class="py-2 px-4 text-left text-gray-200">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($employees as $employee)
+                <tr class="border-t border-gray-700 hover:bg-gray-700">
+                    <td class="py-2 px-4 text-gray-300">{{ $employee->name }}</td>
+                    <td class="py-2 px-4 text-gray-300">{{ $employee->number }}</td>
+                    <td class="py-2 px-4 text-gray-300">{{ $employee->employee_type }}</td>
+                    <td class="py-2 px-4 text-gray-300">{{ $employee->specialization }}</td>
+                    <td class="py-2 px-4">
+                        <div class="flex space-x-2">
+                            <a href="{{ route('employees.show', $employee->id) }}"
+                                class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View</a>
+                            <form action="{{ route('employees.destroy', $employee->id) }}" method="POST"
+                                style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                    class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                    onclick="return confirm('Are you sure you want to delete this employee?')">Delete</button>
+                            </form>
+                        </div>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($employees as $employee)
-                    <tr class="border-t border-gray-700 hover:bg-gray-700">
-                        <td class="py-2 px-4 text-gray-300">{{ $employee->name }}</td>
-                        <td class="py-2 px-4 text-gray-300">{{ $employee->number }}</td>
-                        <td class="py-2 px-4 text-gray-300">{{ $employee->employee_type }}</td>
-                        <td class="py-2 px-4 text-gray-300">{{ $employee->specialization }}</td>
-                        <td class="py-2 px-4">
-                            <div class="flex space-x-2">
-                                <a href="{{ route('employees.show', $employee->id) }}"
-                                    class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View</a>
-                                <form action="{{ route('employees.destroy', $employee->id) }}" method="POST"
-                                    style="display:inline;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit"
-                                        class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                                        onclick="return confirm('Are you sure you want to delete this employee?')">Delete</button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+            @endforeach
+        </tbody>
+    </table>
 
-        <div class="mt-4 text-gray-300">
-            {{ $employees->links() }}
-        </div>
-        </div>
-        <div id="errorContainer" class="py-12 hidden ml-64">
-            <p class="text-red-500">Er kan geen data opgehaald worden.</p>
-        </div>
+    <div class="mt-4 text-gray-300">
+        {{ $employees->links() }}
+    </div>
+    </div>
+    <div id="errorContainer" class="py-12 hidden ml-64">
+        <p class="text-red-500">Er kan geen data opgehaald worden.</p>
+    </div>
 </x-app-layout>
 <script>
     document.getElementById('dataToggle').addEventListener('change', function() {
