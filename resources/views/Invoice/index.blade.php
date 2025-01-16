ae943b7fc93d0ae21e400347156667474a4f6db7
ThomasTadesse <328704@student.mboutrecht.nl>
2025-01-16 11:50:52 +0100

    error handleling invoices index

--- resources/views/Invoice/index.blade.php
+++ resources/views/Invoice/index.blade.php
@@ -20,36 +20,34 @@
             <div class="flex flex-col lg:flex-row gap-8">
                 <!-- Sample list of invoices -->
                 <div class="w-full">
-                    <div class="bg-white shadow-md rounded my-6">
+                    <div class="bg-white shadow-md rounded my-6 overflow-x-auto">
                         <table class="min-w-max w-full table-auto">
-                            <thead>
-                                <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                                    <th class="py-3 px-6 text-left">Invoice #</th>
-                                    <th class="py-3 px-6 text-left">Date</th>
-                                    <th class="py-3 px-6 text-center">Amount</th>
-                                    <th class="py-3 px-6 text-center">Status</th>
-                                </tr>
-                            </thead>
-                            <tbody class="text-gray-600 text-sm font-light">
-                                <tr class="border-b border-gray-200 hover:bg-gray-100">
-                                    <td class="py-3 px-6 text-left whitespace-nowrap">INV-001</td>
-                                    <td class="py-3 px-6 text-left">2023-01-01</td>
-                                    <td class="py-3 px-6 text-center">$100.00</td>
-                                    <td class="py-3 px-6 text-center">Paid</td>
-                                </tr>
-                                <tr class="border-b border-gray-200 hover:bg-gray-100">
-                                    <td class="py-3 px-6 text-left whitespace-nowrap">INV-002</td>
-                                    <td class="py-3 px-6 text-left">2023-01-15</td>
-                                    <td class="py-3 px-6 text-center">$250.00</td>
-                                    <td class="py-3 px-6 text-center">Pending</td>
-                                </tr>
-                                <tr class="border-b border-gray-200 hover:bg-gray-100">
-                                    <td class="py-3 px-6 text-left whitespace-nowrap">INV-003</td>
-                                    <td class="py-3 px-6 text-left">2023-02-01</td>
-                                    <td class="py-3 px-6 text-center">$150.00</td>
-                                    <td class="py-3 px-6 text-center">Overdue</td>
-                                </tr>
-                            </tbody>
+                            @if ($invoices->count() > 0)
+                                <thead>
+                                    <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                        <th class="py-3 px-6 text-left">Invoice #</th>
+                                        <th class="py-3 px-6 text-left">Date</th>
+                                        <th class="py-3 px-6 text-center">Amount</th>
+                                        <th class="py-3 px-6 text-center">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-gray-600 text-sm font-light">
+                                    @foreach ($invoices as $invoice)
+                                        <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                            <td class="py-3 px-6 text-left whitespace-nowrap">{{ $invoice->invoice_number }}</td>
+                                            <td class="py-3 px-6 text-left">{{ $invoice->invoice_date }}</td>
+                                            <td class="py-3 px-6 text-center">{{ $invoice->amount }}</td>
+                                            <td class="py-3 px-6 text-center">{{ $invoice->status }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            @else
+                                <tbody class="text-gray-600 text-sm font-light">
+                                    <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                        <td class="bg-red-500 text-white p-4 rounded mb-4" colspan="4">geen facturen gevonden. Probeer later opnieuw.</td>
+                                    </tr>
+                                </tbody>
+                            @endif
                         </table>
                     </div>
                 </div>
