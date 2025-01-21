ba333a7d7582d2053e7c690bf248405ba1ebfe19
ThomasTadesse <328704@student.mboutrecht.nl>
2025-01-21 10:25:22 +0100

    add invoice number prefix

--- resources/views/Invoice/index.blade.php
+++ resources/views/Invoice/index.blade.php
@@ -35,7 +35,7 @@
                                 <tbody class="text-gray-600 text-sm font-light">
                                     @foreach ($invoices as $invoice)
                                         <tr class="border-b border-gray-200 hover:bg-gray-100">
-                                            <td class="py-3 px-6 text-left whitespace-nowrap">{{ $invoice->number }}</td>
+                                            <td class="py-3 px-6 text-left whitespace-nowrap"># {{ $invoice->number }}</td>
                                             <td class="py-3 px-6 text-left">{{ $invoice->date }}</td>
                                             <td class="py-3 px-6 text-center">€ {{ $invoice->amount }}</td>
                                             <td class="py-3 px-6 text-center">
